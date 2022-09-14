0fe7fcbcc34135b3f266278c1894294dea6ae709
lucille <lucille@lucilles-MacBook-Pro.local>
2022-09-15 00:59:34 +0100

    -

--- src/bio-stronghold/GC.clj
+++ src/bio-stronghold/GC.clj
@@ -1,6 +1,20 @@
 (defn read-fasta [fname]
-  (let [data (clojure.string/split-lines (slurp fname))]
-    (
-))
+  (let [data (clojure.string/split-lines (slurp fname))
+        fasta-parser (fn fapa [[f & rst]]
+                        (let [[l r] (split-with #(not= (first %) \>) rst)]
+                        (cons (vector f (apply str l)) 
+                              (when (seq r)
+                              (lazy-seq (fapa r)))))) ]
+        (fasta-parser data)))
 
-(read-fasta "f")
+(defn gc% [dna]
+   (let [f (frequencies (clojure.string/lower-case dna))
+         gc (+ (get f \g 0) (get f \c 0))]
+        (double (/ gc (count dna)))))
+
+
+(->> (read-fasta "f")
+  (map #(vector (first %) (gc% (second %))))
+  (sort-by second)
+  last
+  println)
